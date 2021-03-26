0f9d2c3ebd9574957b17b27f028b8cbc9d57a29a
Jacob Bare <jacob@parameter1.com>
2021-03-26 08:52:14 -0500

    Create and use carousel image wrapper
    - import the image wrapper into the main carousel component
    - loop over the `images` prop and display each image using the wrapper
    - display the image src, alt, caption and credit within the wrapper

--- sites/overdriveonline.com/browser/my-cool-carousel.vue
+++ sites/overdriveonline.com/browser/my-cool-carousel.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="my-cool-carousel">
     <h3>Vue component: My cool carousel</h3>
-    <ul>
-      <li
-        v-for="image in images"
-        :key="image.id"
-      >
-        {{ image.src }}
-      </li>
-    </ul>
+    <image-wrapper
+      v-for="image in images"
+      :key="image.id"
+      :src="image.src"
+      :alt="image.alt"
+      :caption="image.caption"
+      :credit="image.credit"
+    />
   </div>
 </template>
 
 <script>
+// once inside the container component, sub-components can be directly imported
+import ImageWrapper from './my-cool-carousel/image-wrapper.vue';
+
 export default {
+  components: { ImageWrapper },
+
   props: {
     images: {
       type: Array,
---
+++ sites/overdriveonline.com/browser/my-cool-carousel/image-wrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- eslint-disable vue/no-v-html -->
+  <div class="my-cool-carousel__image-wrapper">
+    <img
+      :src="src"
+      :alt="alt"
+    >
+    <p v-if="caption" v-html="caption" />
+    <small v-if="credit" v-html="credit" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      default: null,
+    },
+    caption: {
+      type: String,
+      default: null,
+    },
+    credit: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
