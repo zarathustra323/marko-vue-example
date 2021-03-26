1ab06254027ea577ff3040001490be51b423ba97
Jacob Bare <jacob@parameter1.com>
2021-03-26 08:42:01 -0500

    Create carousel vue component and add to content page
    - create vue component file
    - regisiter the vue component with the framework’s `Browser` handler
    - load the vue component on the content page via `marko-web-browser-component` and pass the content’s images from the server

--- sites/overdriveonline.com/browser/index.js
+++ sites/overdriveonline.com/browser/index.js
@@ -1,3 +1,11 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 
+/**
+ * Dynamically import the carousel component. This ensures it will only
+ * be included when needed by the page.
+ */
+const MyCoolCarousel = () => import(/* webpackChunkName: "my-cool-carousel" */ './my-cool-carousel.vue');
+
+Browser.register('MyCoolCarousel', MyCoolCarousel);
+
 export default Browser;
---
+++ sites/overdriveonline.com/browser/my-cool-carousel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-cool-carousel">
+    <h3>Vue component: My cool carousel</h3>
+    <ul>
+      <li
+        v-for="image in images"
+        :key="image.id"
+      >
+        {{ image.src }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
